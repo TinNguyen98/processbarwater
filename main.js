9d045432bac2c449c6fc955cf53ba9945a22bb4d
TinNguyen <ntrungtn@gmail.com>
2019-09-23 10:02:03 +0700

    update project processbar water

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+        var elem = document.getElementById("wave");
+        var height = 1;
+        var id = setInterval(frame, 100);
+        var text = document.getElementById("text");
+        function frame() {
+            if (height >= 100) {
+                clearInterval(id);
+            } else {
+                height++;
+                elem.style.height = height + '%';
+                text.innerHTML = height * 1  + '%';
+            }
+        }
+});
+$(document).ready( function() {
+    setTimeout(function(){
+        $("#svg").css("display", "block");
+    },10500);
+});
+
+
